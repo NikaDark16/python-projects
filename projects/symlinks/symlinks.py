268d823c7100304374b2498c79459f7ce2de2acc
IceArrow256 <IceArrow256@gmail.com>
2020-01-02 22:31:40 +0200

    ✨ update symlinks

--- projects/symlinks/symlinks.py
+++ projects/symlinks/symlinks.py
@@ -1,26 +1,34 @@
 import os
 import ia256utilities.filesystem as fs
 
-dr = "C:"
-r = "C:/Users/icearrow256/Dropbox"
+r = "C:"
+dr = "C:/Users/icearrow256/Dropbox"
+gr = "C:/Users/icearrow256/Google Drive"
 
 replacements = {
     "%icearrow256%": "/Users/icearrow256"
 }
 
 def ln(src: str, dst: str):
-    if os.path.isdir(dst):
+    if os.path.exists(dst):
         os.rmdir(dst)
     os.makedirs(src, exist_ok=True)
-    os.system("mklink /J {} {}".format(dst, src))
+    print(src)
+    os.system("mklink /J {} \"{}\"".format(dst, src))
 
 def main():
     symlinks = fs.load_json("symlinks.json", False)
-    for symlink in symlinks:
+    for symlink in symlinks["Dropbox"]:
         for replacement in replacements:
             symlink = symlink.replace(replacement, replacements[replacement])
-        src = os.path.normpath(r + symlink)
-        dst = os.path.normpath(dr + symlink)
+        src = os.path.normpath(dr + symlink)
+        dst = os.path.normpath(r + symlink)
+        ln(src, dst)
+    for symlink in symlinks["Google Drive"]:
+        for replacement in replacements:
+            symlink = symlink.replace(replacement, replacements[replacement])
+        src = os.path.normpath(gr + symlink)
+        dst = os.path.normpath(r + symlink)
         ln(src, dst)
         
 if __name__ == "__main__":
